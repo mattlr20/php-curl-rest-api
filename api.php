2d0ba0a0e2f4cfdc47e0bf8861b0a2d5d37c7ef7
Matthew Richardson <mattlr20@gmail.com>
2018-03-19 16:12:10 -0400

    Update api.php

--- api.php
+++ api.php
@@ -2,18 +2,41 @@
 header("Content-Type:application/json");
 require "data.php";
 
-if(!empty($_GET['name'])) {
-	$name=$_GET['name'];
-	$price = get_price($name);
-	
-	if(empty($price)) {
-		response(200,"Product Not Found",NULL);
-	} else {
-		response(200,"Product Found",$price);
-	}
+switch($_SERVER['REQUEST_METHOD']) {
+	case 'GET': apiGET(); break;
+	case 'PUT': apiPUT(); break;
+	case 'UPDATE': apiUPDATE(); break;
+	case 'DELETE': apiDELETE(); break;
+	default: response(400,"Invalid Request",NULL); break;
+}
 
-} else {
-	response(400,"Invalid Request",NULL);
+function apiGET() {
+	if (isset($_GET['search'])) {
+		$contact = getContacts($_GET['search']);
+		if(empty($contact)) {
+			response(200,"Contact Not Found",NULL);
+		} else {
+			response(200,"Contact Found",$contact);
+		}
+	} else {
+		$contact = getContacts('all');
+		response(200,"All Contacts",$contact);
+	}
+} 
+
+function apiPUT() {
+	$response['data']= $_SERVER['REQUEST_METHOD'];
+	echo json_encode($response);
+}
+
+function apiDELETE() {
+	$response['data']= $_SERVER['REQUEST_METHOD'];
+	echo json_encode($response);
+}
+
+function apiUPDATE() {
+	$response['data']= $_SERVER['REQUEST_METHOD'];
+	echo json_encode($response);
 }
 
 function response($status,$status_message,$data) {
@@ -22,6 +45,7 @@ function response($status,$status_message,$data) {
 	$response['status']=$status;
 	$response['status_message']=$status_message;
 	$response['data']=$data;
+	$response['method']=$_SERVER['REQUEST_METHOD'];
 	
 	$json_response = json_encode($response);
 	echo $json_response;
