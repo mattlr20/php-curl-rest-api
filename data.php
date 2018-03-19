6332c2ee747a01a1cbbaa25d2a9f99c4ac1b204b
Matthew Richardson <mattlr20@gmail.com>
2018-03-19 16:19:14 -0400

    Update data.php

--- data.php
+++ data.php
@@ -1,15 +1,52 @@
 <?php
-function get_price($name) {
-	$products = [
-		"book"=>20,
-		"pen"=>10,
-		"pencil"=>5
+
+function getContacts($search) {
+	$contacts = [
+		[
+			'first_name' => 'Matthew',
+			'last_name' => 'Richardson',
+			'username' => 'matthew.richardson',
+			'email' => 'matthew.richardson@tnc.com'
+		],
+		[
+			'first_name' => 'Jeff',
+			'last_name' => 'Myers',
+			'username' => 'jeff.myers',
+			'email' => 'jeff.myers@tnc.com'
+		],
+		[
+			'first_name' => 'Jamie',
+			'last_name' => 'Brown',
+			'username' => 'jamie.brown',
+			'email' => 'jamie.brown@tnc.com'
+		],
+		[
+			'first_name' => 'Randy',
+			'last_name' => 'Umphlet',
+			'username' => 'randy.umphlet',
+			'email' => 'randy.umphlet@tnc.com'
+		],
+		[
+			'first_name' => 'Johnny',
+			'last_name' => 'Test',
+			'username' => 'johnny.test',
+			'email' => 'johnny.test@tnc.com'
+		],
 	];
-	
-	foreach($products as $product=>$price) {
-		if($product==$name) {
-			return $price;
-			break;
+	$search = strtolower($search);
+	if ($search == 'all') {
+		return $contacts;
+	} else {
+		$contact = [];
+		foreach($contacts as $key => $person) {
+			foreach ($person as $key2 => $info) {
+				if (strpos($info, $search) !== FALSE) {
+					$contact[] = $person;
+					break;
+				}
+			}
 		}
+		return $contact;
 	}
+
 }
